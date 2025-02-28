d7cde051e9cb9fc69bcab92864eb30a832bcb0c4
MarceloTadashi <35063781+MarceloTadashi@users.noreply.github.com>
2025-02-28 10:34:17 +0000

    Adiciona os scripts `lint:check ` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Frase de impacto BOOM!</h1>
+function Home() {
+  return <h1>Frase de impacto BOOM!</h1>;
+}
+
+function teste() {}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
